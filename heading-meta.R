19083eae58ef41ce351791b1a537d57022507206
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-08-12 20:22:03 -0700

    Add p2p figure on the title page

--- heading-meta.R
+++ heading-meta.R
@@ -1,7 +1,7 @@
 metathis::meta() |> 
   metathis::meta_social(
     title = "Futureverse P2P: Peer-to-Peer Parallelization in R",
-    description = "A 25-minute talk on Futureverse P2P",
+    description = "A 18-minute talk on Futureverse P2P",
     url = "https://henrikbengtsson.github.io/talk-user2025-p2p/#/",
     image = "https://raw.githubusercontent.com/HenrikBengtsson/talk-user2025-p2p/main/images/index-img.png",
     image_alt = "The title slide of the presentation, with the future logos",
