537f271c3819966747b060631e9310ff10214ebb
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-08-13 07:40:23 -0700

    Add alt-text

--- heading-meta.R
+++ heading-meta.R
@@ -1,10 +1,10 @@
 metathis::meta() |> 
   metathis::meta_social(
     title = "Futureverse P2P: Peer-to-Peer Parallelization in R",
-    description = "An 18-minute talk on Futureverse P2P",
+    description = "An 18-minute talk titled 'Futureverse P2P: Peer-to-Peer Parallelization in R - Share compute among friends across the world",
     url = "https://henrikbengtsson.github.io/talk-user2025-p2p/#/",
     image = "https://raw.githubusercontent.com/HenrikBengtsson/talk-user2025-p2p/main/images/index-img.png",
-    image_alt = "The title slide of the presentation, with the future logo and an image of the world globe with three connected peer-to-peer users",
+    image_alt = "The title slide of the presentation, with author information, the future logo and an image of the world globe with three connected peer-to-peer users",
     twitter_card_type = "summary_large_image",
     twitter_creator = "@HenrikBengtsson"
   )
